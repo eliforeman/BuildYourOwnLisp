3f2c300bdfdf1eaf6cf142befef1febc82145d47
eliforeman <foreman.e@husky.neu.edu>
2019-11-24 00:47:19 -0500

    Adding support for c preprocessor

--- C_stuff/chap4/repl.c
+++ C_stuff/chap4/repl.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <readline/readline.h>
-#include <readline/history.h>
+
+/* We are now going to use the c preprocessor to determine if we are compiling on windows or not*/
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+
+char *readline(char *prompt){
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+void add_history(char **unused){
+}
+
+
+
+
+#else
+#include <editline/readline.h>
+#endif
+
 
 int main(int argc, char** argv) {
 
